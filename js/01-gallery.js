f0e5f029d824ad9381ff7bb5afc58c4352a5d3b3
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2022-09-23 23:01:03 +0300

    Finished 1.1-1.3 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createGal(array) {
+  return array.reduce((acc, item) =>
+    acc + `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}" rel="noreferrer">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`, "");
+}
+// function createGal(array) {
+//   return array.map((item) => `<div class="gallery__item">
+//   <a class="gallery__link" href="${item.original}" rel="noreferrer">
+//     <img
+//       class="gallery__image"
+//       src="${item.preview}"
+//       data-source="${item.original}"
+//       alt="${item.description}"
+//     />
+//   </a>
+// </div>`).join("");
+// }
+
+ 
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.insertAdjacentHTML('beforeend', createGal(galleryItems));
+
+
+
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="${galleryItems.original}" width="800" height="600">
+// `)
+
+// instance.show()
+
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+function createGal(array) {
+  return array.reduce((acc, item) =>
+    acc + `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`, "");
+}
+
+ 
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.insertAdjacentHTML('beforeend', createGal(galleryItems));
